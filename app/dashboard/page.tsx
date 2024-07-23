9f5efb664997b226c7375d8e5fb3f36078e6ec7e
Anuj Joshi <anujjoshi.ae@gmail.com>
2024-07-23 17:59:18 +0530

    auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,13 @@
-import { signOut, auth } from "@/auth";
+import { auth } from "@/auth";
 
 export default async function Page() {
     const session = await auth();
 
-    const handleSignOut = async () => {
-        "use server";
-        await signOut();
-    };
-
+    
     return (
         <div>
             <h1>Dashboard</h1>
             <pre>{JSON.stringify(session, null, 2)}</pre>
-            <button onClick={handleSignOut}>Sign Out</button>
         </div>
     );
 }
