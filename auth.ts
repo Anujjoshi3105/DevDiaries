ef25770c8397926fcb3117258812fc9d52c01a20
Anuj Joshi <anujjoshi.ae@gmail.com>
2024-07-23 18:32:52 +0530

    auth

--- auth.ts
+++ auth.ts
@@ -8,7 +8,7 @@ import { getUserById } from "@/action/user";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
-  secret: process.env.NEXT_PUBLIC_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/auth/login",
     signOut: "/auth/login",
